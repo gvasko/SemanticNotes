360cb8bceff1a886139d5a87bf0b9c1f304aa408
Gabor Vasko-Szedlar <gabor.vaskoszedlar@gmail.com>
2025-02-09 15:55:33 +0100

    Remove initial generated controller

--- vectornotes.client/src/app/app.component.ts
+++ vectornotes.client/src/app/app.component.ts
@@ -1,12 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
-}
 
 @Component({
   selector: 'app-root',
@@ -15,25 +7,12 @@ interface WeatherForecast {
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  public forecasts: WeatherForecast[] = [];
-  public displayedColumns: string[] = ['date', 'temperatureC', 'temperatureF', 'summary'];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   ngOnInit() {
-    this.getForecasts();
   }
 
-  getForecasts() {
-    this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
-      (result) => {
-        this.forecasts = result;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
 
-  title = 'vectornotes.client';
+//  title = 'Semantic Notes';
 }
