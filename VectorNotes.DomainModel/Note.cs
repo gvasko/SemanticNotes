0c9f210d703320d500861f501e160511c7d094c2
Gabor Vasko-Szedlar <gabor.vaskoszedlar@gmail.com>
2025-03-16 22:04:58 +0100

    Init domain model and persistence

---
+++ VectorNotes.DomainModel/Note.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorNotes.DomainModel
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int OwnerId { get; set; }
+        public User? Owner { get; set; }
+
+        public Note()
+        {
+            Title = "";
+            Content = "";
+        }
+    }
+}
---
+++ VectorNotes.DomainModel/User.cs
@@ -0,0 +1,13 @@
+﻿namespace VectorNotes.DomainModel
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+
+        public User()
+        {
+            Email = "";
+        }
+    }
+}
